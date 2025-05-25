7b42691b3bcb8d2bdd6fbcfb69ed8e12927a5c23
Alec Hance <alecnhance@gmail.com>
2025-05-25 16:15:43 -0400

    added routes for singIn, logIn, landing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,9 @@ import Home from '../pages/Home';
 import Practice from '../pages/Practice';
 import Stats from '../pages/Stats';
 import UserPage from "../pages/UserPage";
+import Landing from "../pages/Landing";
+import SignIn from "../pages/SignIn";
+import LogIn from "../pages/LogIn";
 import '../styles/App.css';
 
 const App = () => {
@@ -19,6 +22,9 @@ const App = () => {
             <Route path="/practice" element={<Practice />} />
             <Route path="/stats" element={<Stats />} />
             <Route path="/user" element={<UserPage />} />
+            <Route path="landing" element={<Landing />} />
+            <Route path="signIn" element={<SignIn />} />
+            <Route path="logIn" element={<LogIn />} />
           </Routes>
         </main>
         <Footer />
