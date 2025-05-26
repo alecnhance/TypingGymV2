b8a79c1214e6351ea1989dd6b61ec6f2208b67a3
Alec Hance <alecnhance@gmail.com>
2025-05-26 09:38:02 -0400

    public pages navigation

---
+++ src/components/landing_components/FreeTyping.jsx
@@ -0,0 +1,9 @@
+const FreeTyping = () => {
+    return (
+        <div>
+            <h2>Free Typing</h2>
+        </div>
+    );
+};
+
+export default FreeTyping;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,11 @@
 import { NavLink } from 'react-router-dom';
+import LandingHeader from '../components/landing_components/LandingHeader';
 
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Landing</h1>
-            <NavLink to="/signIn">
-                Sign In
-            </NavLink>
+        <div className="flex md:flex-row flex-col gap-6 mt-auto mb-auto md:h-[95vh] py-[2vh]">
+            <h2>Landing</h2>
         </div>
     );
 };
