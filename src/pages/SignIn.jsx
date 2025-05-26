a7cba173e29feadf948f00301f2602e856f1f866
Alec Hance <alecnhance@gmail.com>
2025-05-26 08:55:04 -0400

    back button and better loading between clerk pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,23 @@
 import { SignIn } from '@clerk/clerk-react';
 import { NavLink } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
+
 
 const SignInPage = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-mainBackground p-4">
             <div className=" max-w-md flex-col">
+                <div className="mb-3 text-center w-full">
+                    <NavLink 
+                        to="/landing" 
+                        className="text-white hover:text-navOrange hover:underline"
+                    >
+                        <div className='flex items-center'>
+                            <ChevronLeft className={`w-4 h-4 transition-transform `} />
+                            <h2>Back</h2>
+                        </div>
+                    </NavLink>
+                </div>
                 <SignIn
                     signUpUrl="/signUp"
                     appearance={{
@@ -29,14 +42,7 @@ const SignInPage = () => {
                         },
                     }}
                 />
-                <div className="mt-4 text-center w-full">
-                    <NavLink 
-                        to="/landing" 
-                        className="text-white hover:text-navOrange hover:underline"
-                    >
-                        Back
-                    </NavLink>
-                </div>
+                
             </div>
         </div>
     );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,23 @@
 import { SignUp } from '@clerk/clerk-react';
 import { NavLink } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 
 const SignUpPage = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-mainBackground p-4">
             <div className="w-[full max-w-md] flex-col">
+                <div className="mb-3 text-center w-full">
+                    <NavLink 
+                        to="/landing" 
+                        className="text-white hover:text-navOrange hover:underline"
+                    >
+                        <div className='flex items-center'>
+                            <ChevronLeft className={`w-4 h-4 transition-transform `} />
+                            <h2>Back</h2>
+                        </div>
+                    </NavLink>
+                </div>
                 <SignUp
                     signInUrl="/signIn"
                     appearance={{
@@ -27,14 +39,6 @@ const SignUpPage = () => {
                         },
                     }}   
                 />
-                <div className="mt-4 text-center w-full">
-                    <NavLink 
-                        to="/landing" 
-                        className="text-white hover:text-navOrange hover:underline"
-                    >
-                        Back
-                    </NavLink>
-                </div>
             </div>
         </div>
     );
