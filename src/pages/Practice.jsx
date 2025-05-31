756c813cb329a897328a0ce37be3b6e3c3048353
Alec Hance <alecnhance@gmail.com>
2025-05-31 12:54:54 -0400

    fixed practice page main part of page size

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -2,8 +2,10 @@ import TypingArea from "../components/typing_area_comp/TypingArea";
 
 const Practice = () => {
     return (
-      <div className="flex justify-center padding-8">
-        <TypingArea />
+      <div className="flex justify-center padding-8 h-[95vh] py-[2vh]">
+        <div className="flex flex-col w-full items-center">
+          <TypingArea />
+        </div>
       </div>
     );
   };
