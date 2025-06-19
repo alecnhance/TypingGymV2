0150e1f9ec3bbbb404e3258ba2be37ae259fd2ff
Alec Hance <alecnhance@gmail.com>
2025-06-19 19:47:12 -0400

    wpm graph database support

---
+++ src/hooks/useWPMGraph.js
@@ -0,0 +1,46 @@
+import { useEffect, useState, useCallback } from 'react';
+import { useAuth } from '@clerk/clerk-react';
+import { useWebSocket } from '../components/WebSocketProvider';
+import { add } from 'date-fns';
+
+export function useWPMGraph() {
+    const [graph, setGraph] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { getToken } = useAuth();
+    const { data: socketData } = useWebSocket();
+
+    const fetchGraph = useCallback(async () => {
+        setLoading(true);
+        try {
+            const token = await getToken();
+            const res = await fetch('/api/users/me/wpmGraph', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (!res.ok) {
+                console.error("Fetch graph error: ", res.text);
+                return;
+            }
+            const data = await res.json();
+            console.log(data);
+            const formattedData = data.data.reduce((acc, { day, wpm }) => {
+                acc.push({ x: day, y: wpm });
+                return acc;
+            }, [])
+            console.log(formattedData);
+            setGraph(formattedData);
+        } catch (err) {
+            console.error("Error fetching graph data: ", err);
+        } finally {
+            setLoading(false);
+        }
+    }, [getToken]);
+
+    useEffect(() => {
+        fetchGraph();
+    }, [fetchGraph]);
+
+    return { graph, loading };
+}
