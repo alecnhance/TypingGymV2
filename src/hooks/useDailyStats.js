e88237679efdf9b95a47d1f987ec273b922e6f21
Alec Hance <alecnhance@gmail.com>
2025-07-21 19:39:37 -0400

    daily Bar pulls data, just not user score

---
+++ src/hooks/useDailyStats.js
@@ -0,0 +1,46 @@
+import { useState, useCallback, useEffect } from 'react';
+import { useAuth } from '@clerk/clerk-react';
+
+export function useDailyStats() {
+    const [stats, setStats] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { getToken } = useAuth();
+
+    const fetchDailyStats = useCallback(async () => {
+        setLoading(true);
+        try {
+            const token = await getToken();
+            const result = await fetch('api/dailyChallenge/stats', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            if (!result.ok) {
+                console.error("Fetch Daily stats error");
+                return;
+            }
+            const resultData = await result.json();
+            const arr = [];
+            console.log("resultData.arr", resultData.arr)
+            resultData.data.forEach(row => {
+                arr.push({
+                    range: row.range,
+                    count: row.count
+                });
+            });
+            console.log("arr: ", arr);
+            setStats(arr);
+        } catch (err) {
+            console.error("Failed to fetch daily stats: ", err);
+        } finally {
+            setLoading(false);
+        }
+    }, [getToken]);
+
+    useEffect(() => {
+        fetchDailyStats();
+    }, [fetchDailyStats]);
+
+    return { loading, stats }
+}
