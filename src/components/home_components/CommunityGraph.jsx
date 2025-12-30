855504a97686a33889081b602bfadca58ea38c1f
Alec Hance <alecnhance@gmail.com>
2025-12-29 20:43:28 -0500

    backend for dailyLEaders

--- src/components/home_components/CommunityGraph.jsx
+++ src/components/home_components/CommunityGraph.jsx
@@ -1,11 +1,22 @@
-const CommunityGraph = ({ image, username, stat, title }) => {
+import { useDailyLeaders } from '../../hooks/useDailyLeaders';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const CommunityGraph = () => {
+    const { loading, fastestTyper, fastestChallenge, mostPrompts } = useDailyLeaders();
+    if (loading) {
+        return (
+            <div className="flex flex-col w-full h-full items-center justify-center gap-[5%]">
+                <CircularProgress color='white'/>
+            </div>
+        );
+    }
     return (
         <div className="flex flex-col w-full h-full items-center justify-center gap-[5%]">
-            <h2>{title}</h2>
-            <h1>{stat}</h1>
+            <h2>{"Highest Speed Today"}</h2>
+            <h1>{`${Math.round(fastestChallenge.wpm)} WPM`}</h1>
             <div className="flex  h-[15%] items-center gap-[1vw]">
-                <img src={image} className=" h-full aspect-square rounded-full object-cover"/>
-                <h2>{username}</h2>
+                <img src={fastestChallenge.pic_url} className=" h-full aspect-square rounded-full object-cover"/>
+                <h2>{fastestTyper.username}</h2>
             </div>
         </div>
     );
