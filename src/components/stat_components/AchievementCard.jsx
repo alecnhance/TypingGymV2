bf0648832237cb86acc62c08ff5dfab351dcf207
Alec Hance <alecnhance@gmail.com>
2025-12-31 14:54:50 -0500

    updated achievement set

--- src/components/stat_components/AchievementCard.jsx
+++ src/components/stat_components/AchievementCard.jsx
@@ -1,10 +1,10 @@
 import ProgressBar from '../typing_area_comp/ProgressBar';
 
-const AchievementCard = ({ progress, title, description, image }) => {
+const AchievementCard = ({ progress, title, description, icon: Icon }) => {
     return (
         <div className="flex items-center gap-2 h-[70px] flex-grow">
-            <div className='h-[100%] aspect-square object-cover bg-mainBackground p-2 rounded-3xl'>
-                <img src={image} className='w-full h-full object-contain invert'/>
+            <div className='h-[100%] aspect-square object-cover bg-mainBackground p-2 rounded-3xl flex items-center justify-center'>
+                {Icon && <Icon className='w-full h-full text-white' strokeWidth={1.5}/>}
             </div>
             <div className='flex flex-col h-full w-full text-left justify-between py-2'>
                 <p className="text-lg text-white  truncate line-clamp-1">{title}</p>
