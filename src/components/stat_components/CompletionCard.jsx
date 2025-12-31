b4881252ec435b6becad1b9cbc07b5fade0c3d73
Alec Hance <alecnhance@gmail.com>
2025-12-31 14:54:50 -0500

    updated achievement set

--- src/components/stat_components/CompletionCard.jsx
+++ src/components/stat_components/CompletionCard.jsx
@@ -1,10 +1,8 @@
-import dumbbell from '../../assets/dumbbell.svg';
-
-const CompletionCard = ({ image, title, description }) => {
+const CompletionCard = ({ icon: Icon, title, description }) => {
     return (
         <div className="flex items-center gap-2 h-[70px] flex-grow">
-            <div className='h-[100%] aspect-square object-cover bg-navOrange p-2 rounded-3xl'>
-                <img src={image} className='w-full h-full object-contain invert'/>
+            <div className='h-[100%] aspect-square object-cover bg-navOrange p-2 rounded-3xl flex items-center justify-center'>
+                {Icon && <Icon className='w-full h-full text-white' strokeWidth={1.5}/>}
             </div>
             <div className='flex flex-col h-full text-left justify-center py-2'>
                 <p className="text-lg text-white font-bold truncate line-clamp-1">{title}</p>
